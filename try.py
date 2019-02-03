d93839b25e788d04ae95f91b21f91fce1ce24c7b
Jason Kendall <coolacid@gmail.com>
2019-02-03 18:22:52 -0500

    Initial commit

---
+++ try.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import pyax25
+
+x = pyax25.AX25("ve3yca", '192.168.85.3', 10093, 4)
+x.addRelay('VE3LSR')
+# x.setDst("APRS1")
+
+# Usefull Groups:
+# LOCAL: Local news
+# WXBLN: Weather Bulletin
+# AMBER: Amber Alerts
+
+x.sendBulletin("Test Bulletin")
+# x.sendMessage("Test Message")
